03877dea23cabec16c8b02a172b2ce96e73d03c7
yuhiko1122 <i.yuppy@icloud.com>
2023-05-01 14:43:44 +0900

    test

---
+++ js/size.js
@@ -0,0 +1,17 @@
+const wario = document.querySelector("#wario");
+
+wario.addEventListener("click", () => {
+  wario.classList.toggle("scale");
+});
+
+const sonic = document.querySelector("#sonic");
+
+sonic.addEventListener("click", () => {
+  sonic.classList.toggle("scale");
+});
+
+const terry = document.querySelector("#terry");
+
+terry.addEventListener("click", () => {
+  terry.classList.toggle("scale");
+});
