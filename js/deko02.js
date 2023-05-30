9ca0fa18bffd5f4e33d5f45276df7b1d51678eba
yuhiko1122 <i.yuppy@icloud.com>
2023-05-30 11:00:26 +0900

    index and deko

---
+++ js/deko02.js
@@ -0,0 +1,9 @@
+$(".slider").slick({
+  infinite: true, //スライドをループさせるかどうか。初期値はtrue。
+  slidesToScroll: 1, //1回のスクロールで1枚の写真を移動して見せる
+  prevArrow: '<div class="slick-prev"></div>', //矢印部分PreviewのHTMLを変更
+  nextArrow: '<div class="slick-next"></div>', //矢印部分NextのHTMLを変更
+  centerMode: true, //要素を中央ぞろえにする
+  variableWidth: true, //幅の違う画像の高さを揃えて表示
+  dots: true, //下部ドットナビゲーションの表示
+});
